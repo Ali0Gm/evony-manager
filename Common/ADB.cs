55407a41e29f223eff68ff19dcff838904253a12
Ali-Gm <aligolixxx@gmail.com>
2024-07-19 19:21:38 +0330

    ...

---
+++ Common/ADB.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace evony_manager.Common
+{
+    public static class ADB
+    {
+        private static string adbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "adb", "adb.exe");
+        private static string activeDevice = "";
+
+        public static void Connect(string strDeviceID)
+        {
+            ExecuteAdbCommand($"connect {strDeviceID}");
+            activeDevice = strDeviceID;
+        }
+
+        public static void Disconnect(string strDeviceID)
+        {
+            ExecuteAdbCommand($"disconnect {strDeviceID}");
+            activeDevice = "";
+        }
+
+        public static string Command(string strCommand)
+        {
+            return ExecuteAdbCommand(strCommand);
+        }
+
+        public static List<string> DeviceList()
+        {
+            var output = ExecuteAdbCommand("devices");
+            var devices = new List<string>();
+            var lines = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            foreach (var line in lines)
+            {
+                if (line.EndsWith("device"))
+                {
+                    var device = line.Split('\t')[0];
+                    devices.Add(device);
+                }
+            }
+            return devices;
+        }
+
+        public static byte[] CaptureScreenshot()
+        {
+            return ExecuteAdbCommandForBinary("shell screencap -p");
+        }
+
+        private static string ExecuteAdbCommand(string command)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = adbPath;
+            process.StartInfo.Arguments = $"-s {activeDevice} {command}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return result;
+        }
+
+        private static byte[] ExecuteAdbCommandForBinary(string command)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = adbPath;
+            process.StartInfo.Arguments = $"-s {activeDevice} {command}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            using (var ms = new MemoryStream())
+            {
+                process.StandardOutput.BaseStream.CopyTo(ms);
+                process.WaitForExit();
+                return ms.ToArray();
+            }
+        }
+    }
+}
--- Forms/Main.Designer.cs
+++ Forms/Main.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Main";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            timer = new System.Windows.Forms.Timer(components);
+            picDeviceMonitor = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)picDeviceMonitor).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(713, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.Size = new Size(605, 307);
+            textBox1.TabIndex = 1;
+            // 
+            // timer
+            // 
+            timer.Interval = 1000;
+            timer.Tick += timer_Tick;
+            // 
+            // picDeviceMonitor
+            // 
+            picDeviceMonitor.Location = new Point(623, 12);
+            picDeviceMonitor.Name = "picDeviceMonitor";
+            picDeviceMonitor.Size = new Size(165, 307);
+            picDeviceMonitor.TabIndex = 2;
+            picDeviceMonitor.TabStop = false;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(picDeviceMonitor);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Main";
+            Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)picDeviceMonitor).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer;
+        private PictureBox picDeviceMonitor;
     }
 }
